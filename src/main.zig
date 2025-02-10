a4de8dd16bb843817b874f6e1766324a9c455490
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-02-10 17:26:03 -0500

    feat: apply const where we can

--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,11 @@ pub fn main() !void {
     const arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const rawWriter = std.io.getStdOut().writer();
-    var reader = std.io.getStdIn().reader();
+    const reader = std.io.getStdIn().reader();
 
+    // bw cannot be const because bw.writer wants a mutable reference (*Self)
     var bw = std.io.bufferedWriter(rawWriter);
-    var writer = bw.writer();
+    const writer = bw.writer();
     try writer.print("Here we go!\n", .{});
     try bw.flush(); // Don't forget to flush!
     const b = try reader.readByte();
