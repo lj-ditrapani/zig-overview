7902ca853d8fd029cb630d80059d8e991be868af
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2025-02-16 15:37:19 -0500

    test: write some more unit tests

--- src/todo.zig
+++ src/todo.zig
@@ -66,3 +66,91 @@ pub fn processDone(todoList: *ArrayList(Item), arg: []const u8) Result {
         return doneIndexError;
     }
 }
+
+test "todo returns tooMuchInput Result if line is a StreamTooLong error" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const value = todo(&list, error.StreamTooLong, allocator);
+    try std.testing.expectEqual(Result{ .tooMuchInput = {} }, value);
+}
+
+test "todo returns the error if line is any other error" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const value = todo(&list, error.TestError, allocator);
+    try std.testing.expectEqual(error.TestError, value);
+}
+
+test "todo returns unknownCommand if line is null" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const value = todo(&list, null, allocator);
+    try std.testing.expectEqual(unknownCommand, value);
+}
+
+test "todo returns unknownCommand if line is not a command" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const cmd: []u8 = try allocator.dupe(u8, "clap");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(unknownCommand, value);
+}
+
+test "todo returns quit if line is quit" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const cmd: []u8 = try allocator.dupe(u8, "quit");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(Result{ .quit = {} }, value);
+}
+
+test "todo returns help if line is help" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const cmd: []u8 = try allocator.dupe(u8, "help");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(help, value);
+}
+
+test "todo returns emptyListHint if line is list and list is empty" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    const cmd: []u8 = try allocator.dupe(u8, "list");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(emptyListHint, value);
+}
+
+test "todo returns list if line is list and list is non-empty" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    _ = try list.addOne();
+    defer list.deinit();
+    const cmd: []u8 = try allocator.dupe(u8, "list");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(Result{ .list = {} }, value);
+}
+
+test "todo returns missingArg if line is missing arg (add)" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    defer list.deinit();
+    const cmd: []u8 = try allocator.dupe(u8, " add ");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(Result{ .missingArg = .add }, value);
+}
+
+test "todo returns missingArg if line is missing arg (done)" {
+    const allocator = std.testing.allocator;
+    var list = std.ArrayList(Item).init(allocator);
+    defer list.deinit();
+    const cmd: []u8 = try allocator.dupe(u8, " done ");
+    defer allocator.free(cmd);
+    const value = todo(&list, cmd, allocator);
+    try std.testing.expectEqual(Result{ .missingArg = .done }, value);
+}
